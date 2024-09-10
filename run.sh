d76362746fc14b3370dd3a3f1195ec2295b6aca4
Bingsong Zeng <bingsong@usc.edu>
2024-09-10 14:25:12 -0700

    changed screen name

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ MODEL_NAME="${1:-default}"
 echo "Test model: $MODEL_NAME"
 
 echo "Job started"
-screen -dmS bz_pmhc_esm
-screen -S bz_pmhc_esm -X stuff "source env/bin/activate; python main.py -m $MODEL_NAME > output.log 2>&1; deactivate; exit\n"
+screen -dmS bz_pmhc_benchmark
+screen -S bz_pmhc_benchmark -X stuff "source env/bin/activate; python main.py -m $MODEL_NAME > output.log 2>&1; deactivate; exit\n"
