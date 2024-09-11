3b73003fb8a7ba3d8d2a5ce252d7e0694ff26805
Bingsong Zeng <bingsong@usc.edu>
2024-09-10 21:36:32 -0700

    added time for running predictor

--- main.py
+++ main.py
@@ -74,12 +74,15 @@ def main(model_name: str):
         if 'log50k' in df.columns:
             df = df.astype({'log50k': float})
         df = df.groupby('mhc_name')
+        start_time = time.time_ns()
         predictions, labels, log50ks, time_taken = predictor.run(df)
+        end_time = time.time_ns()
         for prediction, task in zip(predictions, predictor.tasks):
             with open(f'{output_dir}/{model_name}/{task}_{filename[:-4]}.txt', 'w') as file:
                 src.test_retrieval(prediction, labels, time_taken, file)
                 if filename in REG_FILES:
                     src.test_regression(prediction, log50ks, file)
+                file.write(f'overall time: {(end_time - start_time)} ns\n')
 
     # test sensitivity
     df = pd.read_csv(f'{data_dir}/pairs.csv')
