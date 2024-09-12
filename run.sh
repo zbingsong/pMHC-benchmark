c2b3a4b3d9b503a8af748322f10ea4b42a39646e
Bingsong Zeng <bingsong@usc.edu>
2024-09-11 20:49:08 -0700

    updated run script

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 MODEL_NAME="${1:-default}"
 echo "Test model: $MODEL_NAME"
+SCREEN_NAME="bz_pmhc_benchmark_$MODEL_NAME"
 
 echo "Job started"
-screen -dmS "bz_pmhc_benchmark_$MODEL_NAME"
-screen -S "bz_pmhc_benchmark_$MODEL_NAME" -X stuff "source env/bin/activate; python main.py -m $MODEL_NAME > output.log 2>&1; deactivate; exit\n"
+screen -dmS "$SCREEN_NAME"
+screen -S "$SCREEN_NAME" -X stuff "source env/bin/activate; python main.py -m $MODEL_NAME > output_$MODEL_NAME.log 2>&1; deactivate; exit\n"
