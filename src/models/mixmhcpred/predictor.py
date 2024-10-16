6138590dcbd688f7c5f806ddcd4bc1d6f70e29bb
Bingsong Zeng <bingsong@usc.edu>
2024-10-16 15:08:12 -0700

    skip empty dataframes

--- src/models/mixmhcpred/predictor.py
+++ src/models/mixmhcpred/predictor.py
@@ -47,6 +47,8 @@ class MixMHCpredPredictor(BasePredictor):
             label = {}
             log50k = {}
             group = group[~group['peptide'].str.contains(r'[BJOUXZ]*', regex=True)].reset_index(drop=True)
+            if len(group) == 0:
+                continue
             grouped_by_len = group.groupby(group['peptide'].str.len())
 
             for length, subgroup in grouped_by_len:
